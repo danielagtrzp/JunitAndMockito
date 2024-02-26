53a63938534280b0b28ca054dd4071ac9cd2903e
danielagtrzp <lgutierrezparra@deloitte.com>
2024-02-26 10:04:02 -0600

    Creating unit test with mock instead stub, stubing a mock SomeDataService

---
+++ src/test/java/com/junitandmockito/SomeBusinessServiceMockTest.java
@@ -0,0 +1,30 @@
+package com.junitandmockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SomeBusinessServiceMockTest {
+
+    @Mock
+    SomeDataService someDataService;
+
+    @InjectMocks
+    SomeBusinessService someBusinessService;
+
+    @Test
+    void testCalculateSum() {
+        given(someDataService.retrieveData()).willReturn(new int[] {10,20})
+                                            .willReturn(new int[]{});
+
+        assertEquals(30, someBusinessService.calculateSum());
+        assertEquals(0, someBusinessService.calculateSum());
+
+    }
+}
--- src/test/java/com/junitandmockito/SomeBusinessServiceTest.java
+++ src/test/java/com/junitandmockito/SomeBusinessServiceTest.java
@@ -1,7 +1,6 @@
 package com.junitandmockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
